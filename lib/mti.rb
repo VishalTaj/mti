953a1cf8ad3fa9b23a74a6c2ae4c0ec72ecbbcee
Vishal Taj <getvishalonline@gmail.com>
2018-11-14 23:13:26 +0800

    moved created and overrided intialize method

--- lib/mti.rb
+++ lib/mti.rb
@@ -5,34 +5,27 @@ module Mti
     attr_accessor :mti_models
 
     def self.has_subclass table
-      @mti_models ||= []
+      @@mti_models ||= []
       
       table_columns = table.to_s.camelize.constantize.column_names
                       .reject {|col| ["id", "#{self.to_s.underscore}_id"].include? col }
                       .compact
 
-      @mti_models << table
+      @@mti_models << table
 
       has_one table, autosave: true, dependent: :destroy
-      
+
       table_columns.each do |col|
         delegate col.to_sym, "#{col}=".to_sym, to: table, allow_nil: true
       end
     end
 
-    def self.create(attributes = nil, options = {}, &block)
-      if attributes.is_a?(Array)
-        attributes.collect { |attr| create(attr, options, &block) }
-      else
-        object = new(attributes, options)
-        @mti_models.map {|model|
-          mti_model_column = model.to_s.camelize.constantize.column_names 
-          object.send("build_#{model.to_s}", attributes.reject {|key, val| val unless mti_model_column.include? key.to_s}) 
-        }
-        yield(object) if block_given?
-        object.save
-        object
-      end
+    def initialize(attributes = nil, options = {})
+      super
+      @@mti_models.map {|model|
+        mti_model_column = model.to_s.camelize.constantize.column_names 
+        self.send("build_#{model.to_s}", attributes.reject {|key, val| val unless mti_model_column.include? key.to_s}) 
+      } unless attributes.nil?
     end
   end
 end
